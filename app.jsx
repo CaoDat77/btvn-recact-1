d8cc7cc99a7bc6749c06475b424518c0ea15bd7e
Cao Dat <tiendat772001@gmail.com>
2022-11-01 16:38:02 +0700

    aaaaa

---
+++ app.jsx
@@ -0,0 +1,50 @@
+function Info() {
+  return (
+    <div className="">
+      <img src="/cus-1.svg" alt="" />;
+      <div className="info">
+        <h3>Susan Smith</h3>
+        <p>WEB DEVELOPER</p>
+      </div>
+    </div>
+  );
+}
+
+function Control() {
+  return (
+    <div className="control">
+      <div className="arow">
+        <i class="bx bx-chevron-left"></i>
+      </div>
+      <div className="arow">
+        <i class="bx bx-chevron-right"></i>
+      </div>
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <div className="">
+      <Info />
+      <p>
+        I'm baby meggings twee health goth +1 . Bicycle rights tumeric
+        chartreuse before the sold out chambray pop-up. Shaman humblebrag
+        pickled coloring book salvia hoodie, cold-pressed four dollar toast
+        everyday carry
+      </p>
+    </div>
+  );
+}
+
+function ReviewCard() {
+  return (
+    <div className="text-center card">
+      <Content />
+      <Control />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<ReviewCard />);
